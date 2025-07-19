177e61cd832232f3be5ce9e84a5a1d950e0969ff
Julliane Tampus <julliane.tampus123@gmail.com>
2025-07-19 22:43:47 +0800

    added add borrwer controller

---
+++ controller/AddBorrowerController.js
@@ -0,0 +1,20 @@
+
+
+
+function add_borrower(borrwer){
+
+    /* 
+        implement add borrwer logic here
+    
+    
+    
+    */
+
+
+    
+
+
+    return console.log("A new borrwer was added.")
+}
+
+module.exports = add_borrower
--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,3 +1,5 @@
+
+
 function auth(user_data){
     console.log("User is Authenticated...")
 }
