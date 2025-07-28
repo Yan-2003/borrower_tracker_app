f6b76ba331e1495b62becd2d18c86ec5f103c066
Julliane Tampus <julliane.tampus123@gmail.com>
2025-07-28 10:46:26 +0800

    convert to local storage for offline use only no api integration for the mean time

--- App.js
+++ App.js
@@ -7,15 +7,55 @@ import AddBorrowerScreen from './screens/AddBorrowerScreen';
 import HistoryScreen from './screens/HistoryScreen';
 import BorrowerScreen from './screens/BorrowerScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import * as SQLite from 'expo-sqlite'
+import { useEffect } from 'react';
+
 
 export default function App() {
 
   const Stack = createStackNavigator()
 
+  const initDB = async () =>{
+
+    const db = await SQLite.openDatabaseAsync('/database/local.db')
+
+    await db.execAsync(`
+      PRAGMA foreign_keys = ON;
+      CREATE TABLE IF NOT EXISTS borrower_tbl (
+        borrwer_id INTEGER PRIMARY KEY NOT NULL,
+        amount DOBULE NOT NULL,
+        interest DOUBLE NOT NULL,
+        time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        total_amount DOBULE NOT NULL,
+        payment_iteration INTEGER NOT NULL,
+        first_name VARCHAR(50) NOT NULL,
+        middle_name VARCHAR(50),
+        last_name VARCHAR(50) NOT NULL,
+      );
+
+      CREATE TABLE IF NOT EXISTS borrower_payment_tbl (
+       borrwer_payment_id INTEGER PRIMARY KEY NOT NULL,
+       borrwer_id INTEGER,
+       FOREIGN KEY(borrwer_id) REFERENCES borrower_tbl(borrower_id)
+      ); 
+    `).then(results => {
+      return console.log(results)
+    });
+  }
+
+
+  useEffect(() => {
+    
+    initDB()
+  
+    return () => {
+      
+    }
+  }, []);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown : false }}>
+      <Stack.Navigator screenOptions={{ headerShown : false }} initialRouteName='Dashboard' >
         <Stack.Screen name='Login' component={LoginScreen} /> 
         <Stack.Screen name='Register' component={RegisterScreen} /> 
         <Stack.Screen name='Dashboard' component={DashboardScreen} /> 
